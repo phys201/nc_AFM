9062eb677e8701de781754589f68f2a02bfae8a1
aspitzig0 <60667685+aspitzig0@users.noreply.github.com>
2022-04-27 12:55:42 -0400

    fixed unittests, added real data fitting
    clened up a few things, added fitting to spherical model.
    unittest for models is working again. Need to add more

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(name='ncafm',
       version='1.0',
-      description='An package for Harvard PHYS201',
+      description='An package for Harvard PHYS201 to fit frequency shift ncAFM data',
       long_description=readme(),
       url='http://github.com/phys201/nc_AFM',
       author='Alyson Spitzig', 'Houchen Li', 'Talha Rehman',
       author_email='alyson.spitzig@gmail.com', 'houchenli@g.harvard.edu', 'talha@g.harvard.edu',
       license='GPLv3',
       packages=['ncafm'],
+      test_suite='tests',
       install_requires=['numpy', 'pymc3', 'arviz', 'seasborn', 'nose'])
